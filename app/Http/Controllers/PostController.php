4ef469841710a7cba0d96ce519c1a25d9e038d11
smsaidurrahmanrobin <smsaiyeed@gmail.com>
2022-03-07 13:09:08 +0600

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return  "This is PostController.php in the index method and the id is";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       return "This is in show method & id is: ". $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "This is edit method";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact(){
+
+      return view('contact');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::resource('/post',PostController::class );
+
+Route::get('/contact',PostController::class . '@contact');
+
+//Route::get('/about', function () {
+//    return 'Hi about page';
+//
+//});
+//
+//Route::get('/contact', function () {
+//    return view('contact page');
+//
+//});
+//
+//Route::get('/post/{id}/{name}', function ($id, $name){
+//
+//    return "This is post no:" .$id."  ". $name;
+//
+//
+//});
+//
+//Route::get('admin/posts/',array('as'=>'admin.home' ,function (){
+//
+//$url = route('admin.home');
+//return "this url is: " . $url;
+//}));
+//
+//
+//
+//Route::group(['middleware' => ['web']], function () {
+//
+//
+//
+//});
